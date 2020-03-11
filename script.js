15d9bb9950e72b31eb1a0acec2f529f8e41e6187
hayjmuir <haymuir@gmail.com>
2020-03-10 21:55:41 -0700

    updated more of the JS

--- script.js
+++ script.js
@@ -1,5 +1,36 @@
 
+ var gifStorage = []
+function renderGif(gifInput){
 
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gifInput +"&api_key=OF6EF6IbhPMflLOCnINThJ4NqdBecwH6&limit=5";
+   
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+
+    .then(function(response){
+        var results = response.data
+        
+        for(var i = 0; i < results.length; i ++){
+
+           
+            
+            var gifDiv = $("<div>")
+            var  rating = results[i].rating;
+            var p = $("<p>").text("Rating: " + rating)
+            var gifs = $("<img>");
+
+            gifs.attr("src", results[i].images.fixed_height.url);
+            gifs.attr("alt" , "gifs");
+
+            gifDiv.prepend(p);
+            gifDiv.prepend(gifs)
+            $("#images").prepend(gifDiv);
+        }
+    });
+};
 
 
 
@@ -7,31 +38,31 @@
 $("button").on("click", function(){
 
 var gifInput = $("#gif-input").val().trim()
-var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gifInput +"&api_key=OF6EF6IbhPMflLOCnINThJ4NqdBecwH6&limit=5";
-var hockeyGIF = []
-
-$.ajax({
-    url: queryURL,
-    method: "GET"
-})
-
-.then(function(response){
-    var results = response.data
-    
-for(var i = 0; i < results.length; i ++){
-
-    var gifDiv = $("<div>")
-    var  rating = results[i].rating;
-    var p = $("<p>").text("Rating: " + rating)
-    var hockeyImg = $("<img>");
-
-    hockeyImg.attr("src", results[i].images.fixed_height.url);
-    hockeyImg.attr("alt" , "hockey image");
-
-    gifDiv.prepend(p);
-    gifDiv.prepend(hockeyImg)
-    $("#images").prepend(gifDiv);
-}
-})
-
+renderGif(gifInput);
+gifStorage.push(gifInput)
+renderButtons()
 });
+
+
+
+function renderButtons() {
+
+$("#btnStorage").empty();
+
+    for(var i = 0; i < gifStorage.length; i ++) {
+        
+        var  newBtn = $("<button>");
+        newBtn.addClass("gif-btn");
+        newBtn.attr("data-name" , gifStorage[i]);
+        newBtn.text(gifStorage[i]);
+        $("#btnStorage").append(newBtn)
+
+
+    }
+
+
+
+
+
+
+}
